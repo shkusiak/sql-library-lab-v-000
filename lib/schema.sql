827d7855886836d453054b2bdb73d308f746a47b
Shea Kusiak <shkusiak@gmail.com>
2018-06-23 19:44:47 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,25 +1,32 @@
 CREATE TABLE characters  (
   id INTEGER PRIMARY KEY,
-  name TEXT, 
-  motto TEXT, 
+  name TEXT,
+  motto TEXT,
   species TEXT,
-  author_id INTEGER, 
+  author_id INTEGER,
   series_id INTEGER
 );
 
 CREATE TABLE books (
   id INTEGER PRIMARY KEY,
-  
+  title TEXT,
+  year INTEGER, 
+  series_id INTEGER
 );
+
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
-  
+  title TEXT, 
+  author_id INTEGER, 
+  sub_genre_id INTEGER
 );
+
 CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
-  
+  name TEXT
 );
+
 CREATE TABLE sub_genre (
   id INTEGER PRIMARY KEY,
-  
+  name TEXT
 );
